6ca09e48d00f1d2b6257bdf8ec7665764d2e6a17
Samuel-Graham <samgraham@outlook.com>
2014-03-13 18:51:52 -0600

    Changed the comments for Larson

--- src/Run.java
+++ src/Run.java
@@ -1,11 +1,9 @@
-// package <none-by-intention>; 
-
 /**
-* The main entry point for the program.
-*
-* Could include displaying a splash screen,
-* or other incidentals.
-*/
+ * The main entry point for the program.
+ *
+ * Could include displaying a splash screen,
+ * or other incidentals.
+ */
 public class Run
 {
    public static void main(String[] args)
