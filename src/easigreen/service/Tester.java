fcacd72fc77febb549d6ecd344a70d41d6e1b215
Samuel-Graham <samgraham@outlook.com>
2014-03-18 09:10:02 -0600

    Added Comments, renamed getStats to printStats

--- src/easigreen/service/Tester.java
+++ src/easigreen/service/Tester.java
@@ -1,12 +1,22 @@
 public class Tester
 {
+   /**
+    * Holds a game
+    */
    private Simulator game;
    
+   /**
+    * Main Class for the tester
+	* @param args the command line arguments
+	*/
    public static void main(String[] args)
    {
       new Tester().run();
    }
 
+   /**
+    * Runs the  method
+	*/
    public void run()
    {
       game = new Simulator();
@@ -22,7 +32,10 @@ public class Tester
       getStats();
    }
 
-   public void getStats()
+   /**
+    * Prints out the Status to the command line
+    */
+   public void printStats()
    {
       System.out.println("");
       System.out.println("Budget:      " + game.getBudget());
