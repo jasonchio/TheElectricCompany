2f8d82858e9249e7218dddb0b0ba58c8e92218bc
Samuel-Graham <samgraham@outlook.com>
2014-03-18 15:52:48 -0600

    Changes as per previous meeting

---
+++ src/easigreen/service/Renewable.java
@@ -0,0 +1,49 @@
+/**
+ * This class contains the default values for an unmodified renewable plant.
+ * It will assign these values unless they are passed into the constructor.
+ *
+ * @version 0.1.1
+ * @author Larson Caldwell
+ * @author Sam Graham
+ * @author Haru McClellan
+ */
+public class Renewable
+    extends Plant
+{
+    //////////////////// Constructors
+
+    /**
+     * Sets default values to a renewable plant
+     */
+    public Renewable()
+    {
+        mAmount = 140;
+        mCostBuild = .2;
+        mCostRemove = -.1;
+        mApproval = 35;
+        mProduction = .1;
+        mEmissions = .05;
+        mSecurity = .03;
+        mProfitPercent = 20;
+    }
+
+    /**
+     * Sets passed values to a renewable plant.
+     *
+     * @param pAmount number of plants
+     * @param pCost cost of each new plant
+     * @param pApproval approval rating per unit
+     * @param pProduction power production per unit
+     * @param pEmissions emissions produced per unit
+     * @param pSecurity the security of each unit
+     * @param pProfitPercent the profit made with each unit
+     */
+    public Renewable(int pAmount, double pCostBuild, double pCostRemove,
+                     double pApproval, double pProduction,
+                     double pEmissions, double pSecurity,
+                     double pProfitPercent)
+    {
+        super (pAmount, pCostBuild, pCostRemove, pApproval, pProduction,
+               pEmissions, pSecurity, pProfitPercent);
+    }
+}
