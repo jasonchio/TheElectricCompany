74199eb483dceb81748439e3e308985e9d68c278
Rick Neff <NeffR@byui.edu>
2014-03-19 15:36:42 -0600

    BOODPAPs #1 and #2 adherence.

--- src/easigreen/desktop/ContentPane.java
+++ src/easigreen/desktop/ContentPane.java
@@ -1,13 +1,83 @@
 package easigreen.desktop;
 
+import easigreen.service.*;
+
+import java.util.*;
+
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
+
+import javafx.event.*;
+import javafx.geometry.*;
+import javafx.scene.*;
+import javafx.scene.image.*;
+import javafx.scene.text.*;
 
 /**
  * A Panel for the center data region of the GUI interface
  */
-class ContentPane extends GridPane
+public class ContentPane
+   extends GridPane
 {
-    public void ContentPane()
-    {
-    }
+   /**
+    * Holds a reference to the model
+    */
+   protected SimCity mModel;
+
+   protected int[] getColumns()
+   {
+      return new int[] {12, 76, 12};
+   }
+
+   protected int[] getRows()
+   {
+      return new int[] {16, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 32};
+   }
+
+   /**
+    * Constructor
+    * @param SimCity pModel the current sim city Model
+    */
+   public ContentPane(SimCity pModel)
+   {
+       mModel = pModel;
+       init();
+       setup();
+   }
+
+   protected void setup()
+   {
+      setGrid();
+      Label title = new Label(getTitle());
+      title.setFont(new Font("Arial", 40));
+      setHalignment(title, HPos.CENTER);
+      add(title, 1, 0);
+   }
+
+   protected String getTitle()
+   {
+      return "Unknown";
+   }
+
+   protected void init()
+   {
+   }
+
+   protected void setGrid()
+   {
+      int[] columns = getColumns();
+      for (int i = 0; i < columns.length; i++)
+      {
+         ColumnConstraints column = new ColumnConstraints();
+         column.setPercentWidth(columns[i]);
+         getColumnConstraints().add(column);
+      }
+      int[] rows = getRows();
+      for (int i = 0; i < rows.length; i++)
+      {
+         RowConstraints row = new RowConstraints();
+         row.setPercentHeight(rows[i]);
+         getRowConstraints().add(row);
+      }
+   }
 }
