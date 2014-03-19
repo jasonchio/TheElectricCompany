870ea4c2048b12e68f5b24f186e2636fce5b0230
Samuel-Graham <samgraham@outlook.com>
2014-03-18 21:25:40 -0600

    New addition from the meeting earlier

---
+++ src/easigreen/service/TechnologyManager.java
@@ -0,0 +1,32 @@
+/**
+ * This is the manager for the technology classes
+ *
+ *
+ * @version 0.1.0
+ * @author Larson Caldwell
+ * @author Sam Graham
+ * @author Haru McClellan
+ */
+public class TechnologyManager
+{
+    //////////////////// Variables
+
+    /**
+     * Daily consumption of oil
+     */
+    private List<Technology> mImplemented;
+
+    /**
+     * Security rating of oil
+     */
+    private List<Technology> mAvailable;
+
+    /**
+     * Oil growth per round
+     */
+    private List<Technology> mAll;
+
+    //////////////////// Constructors
+
+    
+}
