5936b7d94ce4c42e932a4e14c569d77947908e6f
larsoncaldwell <cal09016@byui.edu>
2014-03-19 13:47:48 -0600

    a new sim city

---
+++ src/easigreen/service/WorldManager.java
@@ -0,0 +1,19 @@
+package easigreen.service;
+
+/**
+ * The ResourceManager Manages Resources
+ *
+ * @version 0.1.2
+ * @author Larson Caldwell
+ * @author Sam Graham
+ * @author Haru McClellan
+ */
+public class WorldManager
+{
+    //////////////////// Variables
+
+    /**
+     * The total amount of funds (in billions)
+     */
+    private double mBudget;
+}
