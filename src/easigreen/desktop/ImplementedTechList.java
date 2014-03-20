829f8b9a9ed5581d41d584e8a56338b4931af9a3
Haru McClellan <haruphoenix@gmail.com>
2014-03-20 10:32:03 -0600

    Created tech list that displays all of the Implemented techs in the model

---
+++ src/easigreen/desktop/ImplementedTechList.java
@@ -0,0 +1,24 @@
+package easigreen.desktop;
+
+import easigreen.desktop.*;
+import easigreen.service.*;
+
+import java.util.*;
+
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+import javafx.scene.control.ListView;
+
+class ImplementedTechList
+    extends AppDataList
+{
+
+    public ImplementedTechList(SimCity pModel)
+    {
+	super(pModel);
+    }
+
+    protected void update()
+    {
+    }
+}
