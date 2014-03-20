7c67bf1438bd2120af82aeff84218342d93e6668
Haru McClellan <haruphoenix@gmail.com>
2014-03-20 10:31:36 -0600

    Created tech list that displays all of the available techs in the model

---
+++ src/easigreen/desktop/AvailableTechList.java
@@ -0,0 +1,24 @@
+package easigreen.desktop;
+
+import easigreen.desktop.*;
+import easigreen.service.*;
+
+import java.util.*;
+
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+import javafx.scene.control.ListView;
+
+class AvailableTechList
+    extends AppDataList
+{
+
+    public AvailableTechList(SimCity pModel)
+    {
+	super(pModel);
+    }
+
+    protected void update()
+    {
+    }
+}
