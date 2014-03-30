7272882b1dd06fe0b84ac4ce0160027fcbf84d6e
larsoncaldwell <cal09016@byui.edu>
2014-03-29 18:04:25 -0600

    added comments

--- src/easigreen/service/EnergyManager.java
+++ src/easigreen/service/EnergyManager.java
@@ -17,6 +17,9 @@ public class EnergyManager
      */
     private UpgradeManager mUpgradeManager;
 
+    /**
+     * The upgrades
+     */
     private UpgradeMerger mUpgrades;
 
     /**
@@ -39,6 +42,13 @@ public class EnergyManager
      */
     private OilManager mOil;
 
+    //////////////////// Constructor
+
+    /**
+     * All the array lists are set up and wired to the upgrade manager.
+     *
+     * @param pUpgradeManager the upgrade manager
+     */
     public EnergyManager(UpgradeManager pUpgradeManager)
     {
         mUpgradeManager = pUpgradeManager;
@@ -49,60 +59,105 @@ public class EnergyManager
 	update();
     }
 
+    /**
+     * Gets the nuclear plants
+     *
+     * @return mNuclear the nuclear plants
+     */
     public NuclearManager getNuclear()
     {
         return mNuclear;
     }
 
+    /**
+     * Gets the fossil plants
+     *
+     * @return mFossil the fossil plants
+     */
     public FossilManager getFossil()
     {
         return mFossil;
     }
 
+    /**
+     * Gets the renewable plants
+     *
+     * @return mRenewable the renewable plants
+     */
     public RenewableManager getRenewable()
     {
         return mRenewable;
     }
 
+    /**
+     * Gets the oil data
+     *
+     * @return mOil the oil data
+     */
     public OilManager getOil()
     {
         return mOil;
     }
 
+    /**
+     * Applies upgrade and technology effects to the plants and oil
+     */
     public void update()
     {
 	mUpgrades = mUpgradeManager.update();
     }
 
+
+    /**
+     * Gets the total power produced with all the plants together
+     */
     public double getPower()
     {
 	return mNuclear.getTotalPower() + mFossil.getTotalPower() + mRenewable.getTotalPower();
     }
 
+    /**
+     * Gets the security of all the plants together
+     */
     public double getSecurity()
     {
 	return mNuclear.getTotalSecurity() + mFossil.getTotalSecurity() +
 	    mRenewable.getTotalSecurity() + mOil.getSecurity();
     }
 
+    /**
+     * Gets the amount of all the plants together
+     */
     public int getAmount()
     {
-	return mNuclear.getAmount() + mFossil.getAmount() + mRenewable.getAmount();
+	return mNuclear.getAmount() + mFossil.getAmount() 
+           + mRenewable.getAmount();
     }
 
+    /**
+     * Gets the approval of all the plants together
+     */
     public double getApproval()
     {
-	return ((mNuclear.getApproval() + mFossil.getApproval() + mRenewable.getApproval()) / (double)getAmount());
+	return ((mNuclear.getApproval() + mFossil.getApproval() 
+           + mRenewable.getApproval()) / (double)getAmount());
     }
 
+    /**
+     * Gets the emissions of all the plants together
+     */
     public double getEmissions()
     {
 	return mNuclear.getTotalEmissions() + mFossil.getTotalEmissions() + 
 	    mRenewable.getTotalEmissions() + mOil.getEmissions();
     }
 
+    /**
+     * Gets the security of all the plants together
+     */
     public double getProfit()
     {
-	return mNuclear.getTotalProfit() + mFossil.getTotalProfit() + mRenewable.getTotalProfit();
+	return mNuclear.getTotalProfit() + mFossil.getTotalProfit() 
+           + mRenewable.getTotalProfit();
     }
 }
